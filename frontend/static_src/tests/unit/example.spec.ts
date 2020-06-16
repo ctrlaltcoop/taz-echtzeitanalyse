7811b0bd36a80e88894017036ec44770077f362b
Jonatan Zint <jonatan@ctrl.alt.coop>
2020-06-16 17:30:53 +0200

    Fixing test setup and configuring CI to execute them
    In this commit i fixed the setup for e2e tests via nightwatch and the unit tests via jest.
    Added gitlab-ci.yml to have them executed on CI. The environment that executes the e2e tests need a chrome version
    installed matching the chromedriver being installed via npm. Pinning both for CI is an outstanding issue.
    
    refs #356 #358

--- frontend/static_src/tests/unit/example.spec.ts
+++ frontend/static_src/tests/unit/example.spec.ts
@@ -1,12 +1,22 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import Dashboard from '@/views/Dashboard.vue'
+import VueRouter from 'vue-router'
+import Vue from 'vue'
 
-describe('HelloWorld.vue', () => {
+describe('Dashboard.vue', () => {
+  let router: VueRouter
+  let localVue: typeof Vue
+  beforeEach(() => {
+    localVue = createLocalVue()
+    localVue.use(VueRouter)
+    router = new VueRouter()
+  })
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+    router = new VueRouter()
+    const wrapper = shallowMount(Dashboard, {
+      localVue,
+      router
     })
-    expect(wrapper.text()).toMatch(msg)
+    expect(wrapper.text()).toContain('die echtzeitanalyse')
   })
 })
