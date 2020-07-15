428d8fcdf7d2c660ad38ecb8fd3173d636b53a4f
Lyudmila Vaseva <lyudmila@ctrl.alt.coop>
2020-07-15 17:38:19 +0200

    Use correct indentation according to flake8

--- tazboard/api/queries/devices.py
+++ tazboard/api/queries/devices.py
@@ -3,65 +3,65 @@ from tazboard.api.queries.constants import KEY_FINGERPRINT_AGGREGATION, KEY_DEVI
 
 
 def get_devices_query(min_date, max_date='now', msid=None):
-  query = {
-    "aggs": {
-      KEY_DEVICES_AGGREGATION: {
-        "terms": {
-          "field": "deviceclass",
-          "order": {
-            "_count": "desc"
-          },
-          "size": 10
-        },
+    query = {
         "aggs": {
-          KEY_FINGERPRINT_AGGREGATION: {
-            "cardinality": {
-              "field": "fingerprint"
+            KEY_DEVICES_AGGREGATION: {
+                "terms": {
+                    "field": "deviceclass",
+                    "order": {
+                        "_count": "desc"
+                    },
+                    "size": 10
+                },
+                "aggs": {
+                    KEY_FINGERPRINT_AGGREGATION: {
+                        "cardinality": {
+                            "field": "fingerprint"
+                        }
+                    }
+                }
             }
-          }
-        }
-      }
-    },
-    "size": 0,
-    "docvalue_fields": [
-      {
-        "field": "@timestamp",
-        "format": "date_time"
-      }
-    ],
-    "query": {
-      "bool": {
-        "filter": [
-          {
-            "range": {
-              "@timestamp": {
-                "gte": min_date,
-                "lte": max_date
-              }
+        },
+        "size": 0,
+        "docvalue_fields": [
+            {
+                "field": "@timestamp",
+                "format": "date_time"
             }
-          }
         ],
-        "must_not": [
-          {
-            "match_phrase": {
-              "reloaded": "true"
+        "query": {
+            "bool": {
+                "filter": [
+                    {
+                        "range": {
+                            "@timestamp": {
+                                "gte": min_date,
+                                "lte": max_date
+                            }
+                        }
+                    }
+                ],
+                "must_not": [
+                    {
+                        "match_phrase": {
+                            "reloaded": "true"
+                        }
+                    },
+                    {
+                        "match_phrase": {
+                            "tazlocal": "true"
+                        }
+                    },
+                    {
+                        "match_phrase": {
+                            "device": {
+                                "query": "Spider"
+                            }
+                        }
+                    }
+                ]
             }
-          },
-          {
-            "match_phrase": {
-              "tazlocal": "true"
-            }
-          },
-          {
-            "match_phrase": {
-              "device": {
-                "query": "Spider"
-              }
-            }
-          }
-        ]
-      }
+        }
     }
-  }
-  query = maybe_add_msid_filter(msid, query)
-  return query
+    query = maybe_add_msid_filter(msid, query)
+    return query
