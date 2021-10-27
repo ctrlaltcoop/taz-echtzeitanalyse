b4b3708db1c87391c16d54357738b1c8553c8487
Lyudmila Vaseva <lyudmila@ctrl.alt.coop>
2021-10-27 13:12:52 +0200

    Allow `/total` to be filtered by msid

--- tazboard/api/queries/total.py
+++ tazboard/api/queries/total.py
@@ -1,9 +1,9 @@
 from django.utils import timezone
 
-from tazboard.api.queries.common import get_interval_filter_exclude_bots, get_ranges
+from tazboard.api.queries.common import get_interval_filter_exclude_bots, get_ranges, maybe_add_msid_filter
 
 
-def get_total_query(min_date, max_date=timezone.now()):
+def get_total_query(min_date, max_date=timezone.now(), msid=None):
     min_date_previous_interval = min_date - (max_date - min_date)
     query = {
         "aggs": {
@@ -13,4 +13,5 @@ def get_total_query(min_date, max_date=timezone.now()):
         },
         "query": get_interval_filter_exclude_bots(min_date_previous_interval, max_date)
     }
+    query = maybe_add_msid_filter(msid, query)
     return query
