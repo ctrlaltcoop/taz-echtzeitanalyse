c45389e8beee4148bd29627eaa19f93c976c8401
Lyudmila Vaseva <lyudmila@ctrl.alt.coop>
2021-11-29 18:40:26 +0100

    Update data transformers for new elastic responses

--- tazboard/api/transformers.py
+++ tazboard/api/transformers.py
@@ -6,7 +6,7 @@ from tazboard.api.queries.common import get_dict_path_safe
 from tazboard.api.queries.constants import KEY_TIMESTAMP_AGGREGATION, KEY_FINGERPRINT_AGGREGATION, \
     KEY_REFERRER_AGGREGATION, KEY_TOPLIST_AGGREGATION, KEY_TIMEFRAME_AGGREGATION, \
     KEY_TREND_AGGREGATION, KEY_DEVICES_AGGREGATION, KEY_FIREPLACE_AGGREGATION, \
-    KEY_SUBJECTS_AGGREGATION, KEY_ARTICLE_COUNT_AGGREGATION
+    KEY_SUBJECTS_AGGREGATION, KEY_ARTICLE_COUNT_AGGREGATION, KEY_METADATA_FIELD_MSID
 from tazboard.api.queries.fireplace import get_fireplace_articles_msids
 from tazboard.api.utils.list import get_index_or_none
 from tazboard.api.utils.metadata import parse_article_metadata
@@ -59,15 +59,15 @@ def _transform_referrer_with_ranges_buckets(referrer_buckets):
 
 def _transform_referrer_buckets(referrer_buckets):
     total = reduce(
-        lambda acc, x: acc + x[KEY_TIMEFRAME_AGGREGATION]['doc_count'],
+        lambda acc, x: acc + x[KEY_TIMEFRAME_AGGREGATION]['value'],
         referrer_buckets, 0
     )
     return [
         {
             'referrer': bucket['key'],
-            'hits': bucket[KEY_TIMEFRAME_AGGREGATION]['doc_count'],
+            'hits': bucket[KEY_TIMEFRAME_AGGREGATION]['value'],
             'percentage':
-                bucket[KEY_TIMEFRAME_AGGREGATION]['doc_count'] / total if total > 0 else 0
+                bucket[KEY_TIMEFRAME_AGGREGATION]['value'] / total if total > 0 else 0
         }
         for bucket in referrer_buckets
     ]
@@ -77,7 +77,7 @@ def _transform_device_buckets(device_buckets):
     return [
         {
             'deviceclass': bucket['key'],
-            'hits': bucket[KEY_TIMEFRAME_AGGREGATION]['doc_count']
+            'hits': bucket[KEY_TIMEFRAME_AGGREGATION]['value']
         }
         for bucket in device_buckets
     ]
@@ -148,14 +148,90 @@ def _article_response_to_article_data(article_buckets):
     return data
 
 
-def elastic_toplist_response_to_toplist(es_response):
-    data = _article_response_to_article_data(es_response['aggregations'][KEY_TOPLIST_AGGREGTAION]['buckets'])
-    total = reduce(lambda acc, x: acc + x['hits'], data, 0)
+def elastic_toplist_response_to_toplist(es_response, msids_data):
+    data = _responses_to_article_data(es_response, msids_data['data'])
     total_previous = reduce(lambda acc, x: acc + x['hits_previous'], data, 0)
+    total = reduce(lambda acc, x: acc + x['hits'], msids_data['data'], 0)
     return {
         'total': total,
         'total_previous': total_previous,
         'data': data
+    }
+
+
+def _responses_to_article_data(es_response, msids_data):
+    article_data = {}
+    frontpage_msids = get_fireplace_articles_msids()
+
+    for entry in msids_data:
+        msid = entry['msid']
+        hits = entry['hits']
+
+        headline, kicker, pubtime = parse_article_metadata(msid)
+        url = '{}!{}/'.format(settings.TAZBOARD_TAZ_WEB_URL, msid)
+        archive = False
+        frontpage_index = get_index_or_none(frontpage_msids, msid)
+        if headline is None:
+            headline = url
+            archive = True
+
+        article_partial_data = {
+            'msid': msid,
+            'headline': headline,
+            'url': url,
+            'kicker': kicker,
+            'pubdate': pubtime,
+            'frontpage': msid in frontpage_msids,
+            'archive': archive,
+            'frontpage_position': frontpage_index + 1 if frontpage_index is not None else None,
+            'hits': hits
+        }
+        article_data[msid] = article_partial_data
+
+    for entry in es_response:
+        aggregation = entry['aggregations']
+
+        if KEY_TOPLIST_AGGREGATION in aggregation:
+            msid = get_dict_path_safe(
+                aggregation[KEY_TOPLIST_AGGREGATION], 'meta', KEY_METADATA_FIELD_MSID
+            )
+            hits_previous = aggregation[KEY_TOPLIST_AGGREGATION]['buckets'][0][KEY_TREND_AGGREGATION]['value']
+            article_data[msid]['hits_previous'] = hits_previous
+
+        if KEY_REFERRER_AGGREGATION in aggregation:
+            msid = get_dict_path_safe(
+                aggregation[KEY_REFERRER_AGGREGATION], 'meta', KEY_METADATA_FIELD_MSID
+            )
+            referrers = _transform_referrer_buckets(aggregation[KEY_REFERRER_AGGREGATION]['buckets'])
+            article_data[msid]['referrers'] = referrers
+
+        if KEY_DEVICES_AGGREGATION in aggregation:
+            msid = get_dict_path_safe(
+                aggregation[KEY_DEVICES_AGGREGATION], 'meta', KEY_METADATA_FIELD_MSID
+            )
+            devices = _transform_device_buckets(aggregation[KEY_DEVICES_AGGREGATION]['buckets'])
+            article_data[msid]['devices'] = devices
+
+    return list(article_data.values())
+
+
+def _toplist_msid_response_to_msids(article_buckets):
+    msids_data = []
+    for bucket in article_buckets:
+        msid = get_dict_path_safe(bucket, 'key')
+        hits = bucket[KEY_TIMEFRAME_AGGREGATION]['value']
+        msid_hits = {
+            'msid': msid,
+            'hits': hits
+        }
+        msids_data.append(msid_hits)
+    return msids_data
+
+
+def elastic_toplist_msid_response_to_toplist(es_response):
+    toplist_msids = _toplist_msid_response_to_msids(es_response['aggregations'][KEY_TOPLIST_AGGREGATION]['buckets'])
+    return {
+        'data': toplist_msids
     }
 
 
